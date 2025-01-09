4a49b5fd02ebda52c0faa3ab9903d60c7fbb4f85
Manuel Marzorati <manuel.marzorati1@gmail.com>
2025-01-09 19:38:00 +0100

    Test: cookie error in production

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -33,7 +33,7 @@ router.post('/api/users', async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,         // non accessibile via JavaScript (migliora la sicurezza)
-            secure: process.env.NODE_ENV === 'production',      // Solo su HTTPS in produzione
+            // secure: process.env.NODE_ENV === 'production',      // Solo su HTTPS in produzione
             maxAge: 60 * 60 * 1000 // 1 ora
         });
         res.status(201).send({ message: 'User saved successfully'});
@@ -80,7 +80,7 @@ router.post('/api/users/login', async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,         // non accessibile via JavaScript (migliora la sicurezza)
-            secure: process.env.NODE_ENV === 'production',      // Solo su HTTPS in produzione
+            // secure: process.env.NODE_ENV === 'production',      // Solo su HTTPS in produzione
             maxAge: 60 * 60 * 1000 // 1 ora
         });
         return res.status(200).json({ message: 'Login completed successfully' });
@@ -244,7 +244,7 @@ router.post('/api/users/coins', authMiddleware, async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,         // non accessibile via JavaScript (migliora la sicurezza)
-            secure: process.env.NODE_ENV === 'production',      // Solo su HTTPS in produzione
+            // secure: process.env.NODE_ENV === 'production',      // Solo su HTTPS in produzione
             maxAge: 60 * 60 * 1000 // 1 ora
         });
 
